7cb43c8dfe0dba54cd2cf38863cc4d5b1b519be1
JulyAnahi <july28.ruiz@gmail.com>
2026-06-30 19:46:50 -0300

    cambios

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,3 @@
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-// import { fileURLToPath, URL } from 'node:url' // <-- OBLIGATORIO
-
-// export default defineConfig({
-//   plugins: [vue()],
-//   resolve: {
-//     alias: {
-//       // Le enseña a Vite que '@' es la ruta absoluta hacia la carpeta 'src'
-//       '@': fileURLToPath(new URL('./src', import.meta.url)) 
-//     }
-//   },
-//   server: {
-//     port: 5173,
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:5000',
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, '')
-//       }
-//     }
-//   }
-// })
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
